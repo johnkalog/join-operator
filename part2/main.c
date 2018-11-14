769930ea2533571a7c6759373a9b5102d5192f46
IraklisKts <irakliskotsi97@gmail.com>
2018-11-14 21:07:24 +0200

    search binary files

--- part2/main.c
+++ part2/main.c
@@ -1,14 +1,20 @@
-#include "stdio.h"
-//#include <stdint.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
 //#define __STDC_FORMAT_MACROS
-//#include <inttypes.h>
+#include <inttypes.h>
 #include <string.h>
 
 
 unsigned int line_count(FILE *);
 
 int main(int argc,char *argv[]) {
-    FILE *fp;
+    if(argc<3){
+        printf("Need 2 arguments, '../small/small.init'   and   '../small/'\n");
+        return -1;
+    }
+
+    FILE *fp=NULL,*fp_binary=NULL;
     fp = fopen(argv[1],"r");
 
     //fseek(fp, 0L, SEEK_END);
@@ -20,16 +26,38 @@ int main(int argc,char *argv[]) {
       return -1;
     }
 
-    char * line = NULL;
+    int i,j;
+    char * line = NULL,*path=NULL;
     size_t len = 0;
     ssize_t read;
+    uint64_t num64,num_of_tuples,num_of_cols;
     while((read = getline(&line, &len, fp)) != -1) {
-        printf("Retrieved line of length %zu :\n", read);
-        printf("%s", line);
+        i=0;
+        while(line[i]!='\n')  i++;
+        line[i]='\0';
+
+        path=malloc((strlen(line)+strlen(argv[2])+1)*sizeof(char));
+        strcpy(path,argv[2]);
+        strcat(path,line);
+        fp_binary = fopen(path,"rb");
+        fread(&num_of_tuples,sizeof(uint64_t),1,fp_binary);
+        fread(&num_of_cols,sizeof(uint64_t),1,fp_binary);
+        //  printf("%"PRIu64" - %"PRIu64"\n",num_of_tuples,num_of_cols );
+
+        for(j=0;j<num_of_cols;j++){
+            for(i=0;i<num_of_tuples;i++){
+                fread(&num64,sizeof(uint64_t),1,fp_binary);
+                ///// FILL STRUCTS FILL STRUCTS FILL STRUCTS
+            }
+        }
+
+      /////////////
+      free(path);
+      //////////
+      fclose(fp_binary);
     }
 
 
-    //uint64_t num64;
     //fread(&num64,sizeof(uint64_t),1,fp);
     //printf("%"PRIu64"\n",num64 );
 
