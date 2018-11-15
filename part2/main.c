3ffc7bf0573899c18aba0abda4e80d27bc8e9d78
Ioannis Kalogeropoulos <JohnKal97@gmail.com>
2018-11-15 21:27:13 +0200

    args

--- part2/main.c
+++ part2/main.c
@@ -3,10 +3,10 @@
 
 int main(int argc,char *argv[]) {
 
-    // if(argc!=2){
-    //     printf("Need 1 argument only\n");
-    //     return -1;
-    // }
+    if(argc!=7){
+        printf("Need 1 argument only\n");
+        return -1;
+    }
     unsigned int num_lines;
     full_relation *relations_array = NULL;
     //tuple **tuple_arrays;
